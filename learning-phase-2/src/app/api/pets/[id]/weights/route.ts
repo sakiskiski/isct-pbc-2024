808b2ba11fe6309217f849f590e8a45901adddd9
sakiskiski <saki.6line.27@outlook.jp>
2024-11-16 18:13:48 +0900

    commit

--- learning-phase-2/src/app/api/pets/[id]/weights/route.ts
+++ learning-phase-2/src/app/api/pets/[id]/weights/route.ts
@@ -3,15 +3,15 @@ import { NextResponse } from 'next/server'
 import prisma from '../../../../../../lib/prisma'
 
 // POST /api/pets/:id/weights
-export async function POST(request: Request, { params }: { params: { id: string } }) {
+export async function POST(request: Request) {
   // get id from params
-  const id = params.id
+  // const id = params.id
   // get data from request body
   const data = await request.json()
   // create weight record
   const weight = await prisma.weight.create({
     // data from request body
-    data: data.weight,
+    data: data.weight
   })
   // return Response with weight to json
   return NextResponse.json({ weight })
